a605d4734582529a3b5f3c693d26f6573e7144bf
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-04-14 03:33:51 +0500

    Initialize project using Create React App

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./main/Header";
+import Footer from "./main/Footer";
+
+function App(){
+
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
