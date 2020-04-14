1a94ec6d2dabf30669270ea15e5002f7da2189a9
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-04-14 08:26:17 +0500

    nav bar partially completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,43 @@ import React from "react";
 import Header from "./main/Header";
 import Footer from "./main/Footer";
 
+
 function App(){
 
     return (
         <div>
             <Header />
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+            
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+
+            <div style={{ width:'100%', height: '90px'}}>
+                <h1>Hello I am khamidjon</h1>
+            </div>
+
+
             <Footer />
         </div>
     );
--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,9 +1,55 @@
 import React from "react";
 
-function Header(){
+function Header() {
     return (
         <header>
-            <h1>Khamidjon</h1>
+            <nav class="navbar navbar-expand-lg navbar-light">
+
+                <a class="navbar-brand" href="">Khamidjon</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#footer">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pricing">Skills</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Achievements</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Posts</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Contact</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Posts</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Login</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Sign In</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
         </header>
     );
 }
