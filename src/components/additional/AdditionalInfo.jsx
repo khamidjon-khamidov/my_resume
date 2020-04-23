3672eaf7192f817a20a6b5e559987ce6e29536d1
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-04-23 21:32:46 +0500

    more responsive additional info

--- src/components/additional/AdditionalInfo.jsx
+++ src/components/additional/AdditionalInfo.jsx
@@ -13,53 +13,60 @@ function AdditionalInfo() {
 
 
                 {/* this is Android Development content */}
-                <div className="additional-item-container">
-                    <div className="additional-img-container">
-                        <span className="additional-img">
-                            <i class="fab fa-android"></i>
-                        </span>
-                    </div>
+                <div className="item-container-second">
+                    <div className="additional-item-container">
+
+                        <div className="additional-img-container">
+                            <span className="additional-img">
+                                <i class="fab fa-android" />
+                            </span>
+                        </div>
 
 
-                    <div className="addtional-item-info">
-                        <p className="additional-item1"><strong>Android Development</strong></p>
-                        <br />
-                        <p className="additional-item2">Projects</p>
+                        <div className="addtional-item-info">
+                            <p className="additional-item1"><strong>Android Development</strong></p>
+                            <br />
+                            <p className="additional-item2">Projects</p>
+                        </div>
                     </div>
                 </div>
 
                 {/* this is Algorithms content */}
-                <div style={algoStyle} className="additional-item-container">
-                    <div className="additional-img-container">
-                        <span className="additional-img">
-                            <i class="fas fa-code"></i>
-                        </span>
-                    </div>
+                <div className="item-container-second">
+                    <div style={algoStyle} className="additional-item-container">
+                        <div className="additional-img-container">
+                            <span className="additional-img">
+                                <i class="fas fa-code" />
+                            </span>
+                        </div>
 
 
-                    <div className="addtional-item-info">
-                        <p className="additional-item1"><strong>Algorithms</strong></p>
-                        <br />
-                        <p className="additional-item2">Contestant</p>
+                        <div className="addtional-item-info">
+                            <p className="additional-item1"><strong>Algorithms</strong></p>
+                            <br />
+                            <p className="additional-item2">Contestant</p>
+                        </div>
                     </div>
                 </div>
+
 
                 {/* this is Web Development content */}
-                <div className="additional-item-container">
-                    <div className="additional-img-container">
-                        <span className="additional-img">
-                            <i class="fab fa-internet-explorer"></i>
-                        </span>
-                    </div>
+                <div className="item-container-second">
+                    <div className="additional-item-container">
+                        <div className="additional-img-container">
+                            <span className="additional-img">
+                                <i class="fab fa-internet-explorer"></i>
+                            </span>
+                        </div>
 
 
-                    <div className="addtional-item-info">
-                        <p className="additional-item1"><strong>Web Development</strong></p>
-                        <br />
-                        <p className="additional-item2">Full Stack</p>
+                        <div className="addtional-item-info">
+                            <p className="additional-item1"><strong>Web Development</strong></p>
+                            <br />
+                            <p className="additional-item2">Full Stack</p>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     )
