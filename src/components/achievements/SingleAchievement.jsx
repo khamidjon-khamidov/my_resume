7d5d9a70f4e2f3ae2095ce5a50b6eb6aeed8b990
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-04-27 18:50:56 +0500

    mobile responsiveness

--- src/components/achievements/SingleAchievement.jsx
+++ src/components/achievements/SingleAchievement.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import Roll from "react-reveal/Roll";
-import Flip from "react-reveal/Flip";
 import Zoom from "react-reveal/Zoom";
 
 function SingleAchievement(props) {
@@ -10,37 +8,23 @@ function SingleAchievement(props) {
     let textAreaId = itemId + "txtarea";
     let backgrId = itemId + "backgr";
 
-    const containerStyle = {
-        textAlign: "center",
-        position: "relative",
-        flex: "1 0 100px",
-        margin: "10px",
-        borderStyle: "groove",
-        borderColor: "#fac100",
-        transition: "background 0.4s ease-in-out",
-        WebkitBoxShadow: "0px 0px 21px -5px rgba(0, 0, 0, 0.75)",
-        MozBoxShadow: "0px 0px 21px -5px rgba(0, 0, 0, 0.75)",
-        boxShadow: "0px 0px 21px -5px rgba(0, 0, 0, 0.75)"
-
-    }
-
     const imgContStyle = {
         width: "100%",
         background: "white"
     }
 
     const txtContStyle = {
-        position: "relative",
         background: "white",
-        lineHeight: "0.4",
+        lineHeight: "0",
         padding: "2% 0 0 1%",
+        marginBottom: "0",
         borderStyle: "groove none none none",
         borderColor: "#fac100",
         color: "#a600ff",
         fontSize: "1.5rem",
         fontFamily: "'Hi Melody', cursive",
         bottom: "0",
-        zIndex: "2",
+        // zIndex: "2",
         transition: "background 0.4s ease-in-out"
     }
 
@@ -48,8 +32,9 @@ function SingleAchievement(props) {
         position: "absolute",
         top: "0",
         width: "0",
+        marginBottom: "0",
         zIndex: "1",
-        height: "90%",
+        height: "100%",
         background: "rgba(0, 0, 0, 0.69)",
         transition: "width 0.4s ease-in-out"
     }
@@ -57,6 +42,7 @@ function SingleAchievement(props) {
     function onMouseOvered(e) {
         document.getElementById(itemId).style.width = "100%"
         document.getElementById(textAreaId).style.background = "#fac100";
+        document.getElementById(itemId+"contBackgr").style.background="#fac100";
         document.getElementById(textAreaId).style.color = "white";
         document.getElementById(backgrId).style.background = "#fac100";
     }
@@ -64,37 +50,32 @@ function SingleAchievement(props) {
     function onMouseOuted(e) {
         document.getElementById(itemId).style.width = "0%";
         document.getElementById(textAreaId).style.background = "white";
+        document.getElementById(itemId+"contBackgr").style.background="white";
         document.getElementById(textAreaId).style.color = "#a600ff";
         document.getElementById(backgrId).style.background = "white";
     }
 
 
-    const [isShow, setIsShow] = useState(false);
-
-    setTimeout(function () {
-        setIsShow(true);
-    }, 1000);
-
     return (
-        <div>
-            <Zoom opposite cascade when={isShow}>
+        <div className="single-achievement-cont-style" id={itemId+"contBackgr"}>
+            <Zoom opposite cascade>
                 <div
                     id={backgrId}
                     onMouseOut={onMouseOuted}
                     onMouseOver={onMouseOvered}
-                    style={containerStyle}>
+                    >
 
                     <div id={itemId} style={wraperCont}></div>
 
                     <div style={imgContStyle}>
-                        <img src={itemLink} alt="Img"/>
+                        <img style={{width:"100%"}} src={itemLink} alt="Img"/>
                     </div>
 
                     <div id={textAreaId} style={txtContStyle}>
                         <h1
-                            style={{fontSize:"2rem", lineHeight: "0.8"}}
+                            style={{fontSize:"2rem", lineHeight: "1"}}
                         >{itemTitle}</h1>
-                        <p>{itemDescription}</p>
+                        <p style={{marginBottom:"2px", lineHeight:"1"}}>{itemDescription}</p>
                     </div>
                 </div>
             </Zoom>
