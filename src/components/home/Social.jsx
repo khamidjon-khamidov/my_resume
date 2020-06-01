2cc0f5bfc2ac727a5f300790fac9acdeb9b70ba4
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-06-01 14:15:36 +0500

    done

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -15,7 +15,7 @@ function Social() {
         <div className="home-social">
             <span onClick={()=>onSocialClick("https://github.com/SomeoneAndNoone")} style={iconStyle}><i className="home-social-icon fab fa-github"></i></span>
             <span onClick={()=>onSocialClick("https://www.facebook.com/khamidjon.khamidov")} style={iconStyle}><i className="home-social-icon fab fa-facebook-f"></i></span>
-            <span onClick={()=>onSocialClick("https://www.linkedin.com/in/hamidjon-hamidov-a31128194/")} style={iconStyle}><i className="home-social-icon fab fa-linkedin-in"></i></span>
+            <span onClick={()=>onSocialClick("https://www.linkedin.com/in/george-o-connell-168877162/")} style={iconStyle}><i className="home-social-icon fab fa-linkedin-in"></i></span>
             <span onClick={()=>onSocialClick("https://t.me/work_smard")} style={iconStyle}><i className="home-social-icon fab fa-telegram"></i></span>
         </div>
     )
